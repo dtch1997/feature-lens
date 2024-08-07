519b16f14e0d76b47ec9f06e3f390c9995463521
Daniel Tan <dtch1997@users.noreply.github.com>
2024-08-07 15:01:57 +0100

    WIP implement leap algorithm

--- projects/leap_sfc/leap/leap.py
+++ projects/leap_sfc/leap/leap.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import networkx
 import torch
 
-from einops import einsum, rearrange
+from einops import einsum
 from jaxtyping import Float
 from typing import Iterable
 
@@ -15,42 +15,15 @@ from .model_handler import ModelHandler
 from .utils import iter_upstream_heads
 from .sparse import sparse_mean
 
-
-def compute_direct_path_attribution(
-    model_handler: ModelHandler,
-    cache_handler: CacheHandler,
-    upstream_head: Head,
-    downstream_head: Head,
-) -> Float[torch.Tensor, "batch query_seq down_d_sae key_seq up_d_sae"]:
-    """Compute a score that measures how much an upstream node directly affects a downstream node
-
-    The Direct Path Attribution (DPA) from an upstream feature F to a downstream feature G
-    is the amount by which the activation of G decreases when we ablate the direct-path contribution of F.
-
-    Concretely:
-    - Let a(F) and a(G) be the clean acts of F and G.
-    - Let a’(G) be act of G when we subtract a(F)*decoder_col(F) from the input to the head where G lives.
-    - DPA(F -> G) = a(G) - a’(G)
-    """
-
-    # Get the activations of the downstream node
-    downstream_acts = cache_handler.get_act(upstream_head)
-
-    # Get the activations of the upstream node
-    upstream_acts = cache_handler.get_act(downstream_head)
-
-    # Get the activations of the downstream node when we ablate the direct-path contribution of the upstream node
-    ablated_acts = 0
-    raise NotImplementedError("Need to calculate ablated_acts using encoder and relu")
-
-    return downstream_acts - ablated_acts
+# Define a sparse tensor type annotation
+SparseTensor = torch.Tensor
 
 
 def construct_sparse_grad_input_tensor_for_mlp(
     W_enc: Float[torch.Tensor, "d_model d_sae"],
     input_act: Float[torch.Tensor, "batch seq d_sae"],
-) -> Float[torch.Tensor, "batch seq d_sae d_model"]:
-    """Construct a sparse tensor of d(head_output)/d(node_act) for MLP transcoders
+) -> Float[SparseTensor, "batch seq d_sae d_model"]:
+    """Compute d(node_act)/d(head_input) for MLP transcoders
 
     Inputs:
     - W_enc: [d_model, d_sae] - the encoder weights
@@ -84,10 +57,8 @@ def construct_sparse_grad_input_tensor_for_att(
     W_V: Float[torch.Tensor, "n_head d_head d_model"],
     attn_pattern: Float[torch.Tensor, "batch n_head query_seq key_seq"],
     input_act: Float[torch.Tensor, "batch seq d_sae"],
-) -> Float[torch.Tensor, "batch query_seq key_seq d_sae d_model"]:
-    """Construct a sparse tensor of d(head_output)/d(node_act) for attention-out SAEs
-
-    Calculates d(node_act)/d(head_input) for each node
+) -> Float[SparseTensor, "batch query_seq key_seq down_d_sae d_model"]:
+    """Compute d(node_act)/d(head_input) for attention-out SAEs
 
     Inputs:
     - W_enc: [n_head, d_head, d_sae] - the encoder weights
@@ -113,8 +84,9 @@ def construct_sparse_grad_input_tensor_for_att(
 def get_sae_act_post_grad_head_input(
     model_handler: ModelHandler,
     cache_handler: CacheHandler,
-    downstream_node: Node,
-) -> Float[torch.Tensor, "batch down_layer query_seq down_d_sae d_model"]:
+    downstream_head: Head,
+    downstream_important_nodes: list[Node],
+) -> Float[SparseTensor, "batch query_seq down_d_sae d_model"]:
     """Get the gradient of the post-ReLU activations of the SAE w.r.t the input to that head
 
     Remarks
@@ -122,35 +94,39 @@ def get_sae_act_post_grad_head_input(
     - To avoid blowup in memory, the features under consideration should be filtered
         - e.g. gradients for non-active features will be zero
     """
+    # TODO: Implement filtering by downstream_important_nodes
 
-    if downstream_node.head_type == "att":
+    if downstream_head.head_type == "att":
         # Attention-out SAE
+        # NOTE: This will be encoder weights, multiplied by W_V
+        sae = model_handler.get_sae_for_head(downstream_head)
+        W_enc = sae.W_enc
+        W_V = model_handler.blocks[downstream_head.layer].attn.W_V
+        # Hackily get the attention pattern
+        attn_pattern = cache_handler.get_cache(downstream_head, "clean")[
+            "blocks.0.attn.hook_pattern"
+        ]
+        input_act = cache_handler.get_act(downstream_head)
+        return construct_sparse_grad_input_tensor_for_att(
+            W_enc, W_V, attn_pattern, input_act
+        )
 
-        # NOTE: currently implement only the OV step
-        # Conceptually, this is just (W_enc row) @ W_V @ attention_pattern
-        # - attention pattern from the clean cache
-        # - W_enc from the SAE
-        # - W_V from the attention head
-
-        # TODO: Q, K steps
-        raise NotImplementedError("Need to implement OV step")
-
-    elif downstream_node.head_type == "mlp":
+    elif downstream_head.head_type == "mlp":
         W_enc = model_handler.get_sae_for_head(
-            downstream_node
+            downstream_head
         ).W_enc  # [d_model, d_sae]
         # Get only the active features
-        input_act = cache_handler.get_act(downstream_node)  # [batch, seq, d_sae]
-        sparse_tensor = construct_sparse_grad_input_tensor_for_mlp(W_enc, input_act)
-        return sparse_tensor
+        input_act = cache_handler.get_act(downstream_head)  # [batch, seq, d_sae]
+        return construct_sparse_grad_input_tensor_for_mlp(W_enc, input_act)
+
     else:
-        raise ValueError(f"Unknown head type: {downstream_node.head_type}")
+        raise ValueError(f"Unknown head type: {downstream_head.head_type}")
 
 
 def construct_sparse_grad_output_tensor_for_mlp(
     W_dec: Float[torch.Tensor, "d_model d_sae"],
     input_act: Float[torch.Tensor, "batch seq d_sae"],
-) -> Float[torch.Tensor, "batch seq d_sae d_model"]:
+) -> Float[SparseTensor, "batch key_seq up_d_sae d_model"]:
     """Construct a sparse tensor of d(head_output)/d(node_act) for MLP transcoders"""
     raise NotImplementedError(
         "Need to implement construct_sparse_grad_output_tensor_for_mlp"
@@ -158,21 +134,38 @@ def construct_sparse_grad_output_tensor_for_mlp(
 
 
 def construct_sparse_grad_output_tensor_for_att(
-    W_dec: Float[torch.Tensor, "d_sae n_head d_head"],
-    W_O: Float[torch.Tensor, "n_head d_head d_model"],
-    input_act: Float[torch.Tensor, "batch seq d_sae"],
-):
+    W_dec: Float[torch.Tensor, "d_sae d_z_concat"],
+    W_O: Float[torch.Tensor, "d_z_concat d_model"],
+    input_act: Float[SparseTensor, "batch seq d_sae"],
+) -> Float[SparseTensor, "batch key_seq up_d_sae d_model"]:
     """Construct a sparse tensor of d(head_output)/d(node_act) for attention-out SAEs"""
-    raise NotImplementedError(
-        "Need to implement construct_sparse_grad_output_tensor_for_att"
+    W = einsum(W_dec, W_O, "d_sae d_z_concat, d_z_concat d_model -> d_sae d_model")
+
+    batch, seq, d_sae = input_act.shape
+    d_model = W.shape[1]
+
+    # Find nonzero activations
+    nonzero_mask = input_act != 0
+    nonzero_indices = nonzero_mask.nonzero()
+
+    # Get the corresponding rows from W_enc
+    values = W[:, [idx[2] for idx in nonzero_indices]]
+
+    # Create sparse tensor
+    sparse_tensor = torch.sparse_coo_tensor(
+        indices=nonzero_indices.t(),
+        values=values.t(),
+        size=(batch, seq, d_sae, d_model),
     )
+
+    return sparse_tensor
 
 
 def get_sae_act_post_grad_head_output(
     model_handler: ModelHandler,
     cache_handler: CacheHandler,
     upstream_head: Head,
-) -> Float[torch.Tensor, "batch key_seq up_d_sae d_model"]:
+) -> Float[SparseTensor, "batch key_seq up_d_sae d_model"]:
     """Get the gradient of the output of that head w.r.t the post ReLU activations of the SAE
 
     Remarks
@@ -185,22 +178,12 @@ def get_sae_act_post_grad_head_output(
         # NOTE: This will be decoder weights, multiplied by W_V
         # NOTE: We need to multiply the W_V matrix here manually
         sae = model_handler.get_sae_for_head(upstream_head)
-        W_dec = sae.W_dec  # [d_sae, d_model]
-        W_dec_reshape = rearrange(
-            W_dec,
-            "d_sae d_model -> d_sae n_head d_head",
-            n_head=model_handler.model.cfg.n_heads,
-        )
-        W_V = model_handler.blocks[
+        W_dec = sae.W_dec  # [d_sae, d_z_concat]
+        W_O = model_handler.blocks[
             upstream_head.layer
-        ].attn.W_V  # [n_head, d_head, d_model]
+        ].attn.W_O  # [d_z_concat, d_model]
         input_act = cache_handler.get_act(upstream_head)
-        attn_pattern = cache_handler.get_cache("clean")[
-            f"blocks.{upstream_head.layer}.attn.hook_pattern"
-        ]
-        return construct_sparse_grad_output_tensor_for_att(
-            W_dec_reshape, W_V, attn_pattern, input_act
-        )
+        return construct_sparse_grad_output_tensor_for_att(W_dec, W_O, input_act)
 
     elif upstream_head.head_type == "mlp":
         W_dec = model_handler.get_sae_for_head(upstream_head).W_dec  # [d_sae, d_model]
@@ -211,6 +194,33 @@ def get_sae_act_post_grad_head_output(
 
     else:
         raise ValueError(f"Unknown head type: {upstream_head.head_type}")
+
+
+def get_grad_metric_wrt_act(
+    cache_handler: CacheHandler,
+    downstream_head: Head,
+    downstream_important_nodes: list[Node],
+):
+    grad = cache_handler.get_grad_metric_wrt_act(
+        downstream_head
+    )  # [batch, query_seq, down_d_sae]
+    grad_sparse = grad.to_sparse()
+
+    # Filter grad_sparse by downstream_important_nodes
+    for idx in grad_sparse.indices():
+        _, query_pos, down_feature = idx
+        if (
+            Node(
+                downstream_head.layer,
+                downstream_head.head_type,
+                query_pos,
+                down_feature,
+            )
+            not in downstream_important_nodes
+        ):
+            grad_sparse.values()[idx] = 0
+
+    return grad_sparse
 
 
 class LeapAlgo:
@@ -238,20 +248,37 @@ class LeapAlgo:
 
     def leap_step(self, head: Head):
         downstream_head = head
-        # TODO: Handle filtering downstream head by important features...
-        grad = get_sae_act_post_grad_head_input(
-            self.model_handler, self.cache_handler, downstream_head
+        # TODO: Handle filtering downstream head by important nodes
+        downstream_important_nodes = list(
+            self.iter_important_nodes_at_head(downstream_head)
         )
-        grad /= self.cache_handler.get_layernorm_scale(downstream_head)
+
+        grad_down_act_head_input = get_sae_act_post_grad_head_input(
+            self.model_handler,
+            self.cache_handler,
+            downstream_head,
+            downstream_important_nodes,
+        )
+        grad_down_act_head_input /= self.cache_handler.get_layernorm_scale(
+            downstream_head
+        )
 
         for upstream_head in iter_upstream_heads(downstream_head):
-            dpa = compute_direct_path_attribution(
-                self.model_handler,
-                self.cache_handler,
-                upstream_head,
-                downstream_head,
+            grad_head_output_up_act = get_sae_act_post_grad_head_output(
+                self.model_handler, self.cache_handler, upstream_head
             )
-            metric_grad = self.cache_handler.get_grad_metric_wrt_act(downstream_head)
+            up_act = self.cache_handler.get_act(upstream_head)
+
+            dpa = einsum(
+                up_act,
+                grad_head_output_up_act,
+                grad_down_act_head_input,
+                "batch key_seq up_d_sae, batch key_seq up_d_sae d_model, batch down_layer query_seq down_d_sae d_model, -> batch query_seq down_d_sae key_seq up_d_sae",
+            )
+            metric_grad = get_grad_metric_wrt_act(
+                self.cache_handler, downstream_head, downstream_important_nodes
+            )
+
             # NOTE: This has to be done using sparse tensors due to memory concerns
             ma = einsum(
                 dpa,
@@ -263,8 +290,8 @@ class LeapAlgo:
             ma_batchmean = sparse_mean(ma, dim=0)
 
             # Add the edge if the score is above the threshold
-            for idx, val in zip(ma.indices(), ma.values()):
-                batch, query_pos, down_feature, key_pos, up_feature = idx
+            for idx, val in zip(ma_batchmean.indices(), ma_batchmean.values()):
+                query_pos, down_feature, key_pos, up_feature = idx
                 if val > self.threshold:
                     upstream_node = Node(
                         upstream_head.layer,
@@ -278,7 +305,9 @@ class LeapAlgo:
                         query_pos,
                         down_feature,
                     )
+                    assert downstream_node in self.graph
                     self.graph.add_edge(upstream_node, downstream_node)
+                    # NOTE: the above also adds upstream_node to the graph
 
             # TODO: implement other pruning strategies
             # - Prune by absolute threshold
@@ -289,9 +318,11 @@ class LeapAlgo:
 
         At the end, the graph will contain selected edges
         """
-        for layer in reversed(self.model_handler.n_layers):
+        for layer in reversed(list(range(self.model_handler.n_layers))):
             for head_type in ["mlp", "att"]:
                 # NOTE: "att" currently handles 'OV' circuit only
                 # TODO: implement "q", "k" circuits
                 head = Head(layer, head_type)
                 self.leap_step(head)
+
+        return self
